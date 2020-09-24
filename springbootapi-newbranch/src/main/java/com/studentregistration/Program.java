b9a2cdcb94c1158213b6a1ac03b48724d00996fc
alazarabraham <amarkos@xpanxion.com>
2020-09-24 14:10:50 -0400

    complete backend w relations

---
+++ springbootapi-newbranch/src/main/java/com/studentregistration/Program.java
@@ -0,0 +1,72 @@
+package com.studentregistration;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+ 
+@Entity
+public class Program {
+    private Integer programId;
+    private String programName;
+    private String instructor;
+    private String semester;
+    private String level;
+    
+ 
+    public Program() {
+    }
+ 
+    public Program(Integer programId, String programName,String instructor,String semester,String level) {
+        this.programId = programId;
+        this.programName = programName;
+        this.instructor = instructor;
+        this.semester = semester;
+        this.level = level;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getProgramId() {
+		return programId;
+	}
+
+	public void setProgramId(Integer programId) {
+		this.programId = programId;
+	}
+
+	public String getProgramName() {
+		return programName;
+	}
+
+	public void setProgramName(String programName) {
+		this.programName = programName;
+	}
+
+	public String getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+
+	public String getSemester() {
+		return semester;
+	}
+
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+ 
+   
+    
+}
---
+++ springbootapi-newbranch/src/main/java/com/studentregistration/Student.java
@@ -0,0 +1,105 @@
+package com.studentregistration;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+ 
+@Entity
+public class Student {
+	
+    private Integer studentId;
+    private String firstname;
+    private String lastname;
+    private String degreeProgram;
+    private String DOB;
+    private Integer progId;
+
+    
+ 
+    public Student() {
+    }
+ 
+    public Student(Integer studentId, String firstname,String lastname,String degreeProgram,String DOB) {
+        this.studentId = studentId;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.degreeProgram = degreeProgram;
+
+        this.DOB = DOB;
+
+    }
+    
+    public Student(Integer studentId, String firstname,String lastname,String degreeProgram,String DOB,Integer progId) {
+        this.studentId = studentId;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.degreeProgram = degreeProgram;
+        this.DOB = DOB;
+        this.progId = progId;
+
+
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
+
+	public String getDegreeProgram() {
+		return degreeProgram;
+	}
+
+	public void setDegreeProgram(String degreeProgram) {
+		this.degreeProgram = degreeProgram;
+	}
+
+	public String getDOB() {
+		return DOB;
+	}
+
+	public void setDOB(String dOB) {
+		DOB = dOB;
+	}
+
+	public Integer getProgId() {
+		return progId;
+	}
+
+	public void setProgId(Integer progId) {
+		this.progId = progId;
+	}
+
+	
+    
+    
+
+	
+ 
+    
+
+}
